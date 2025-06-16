9884a5f9b985d70580966fefb25e661d908489f9
Santiago Cardona Herrera <santiago.cardona-herrera@flyr.com>
2025-06-16 14:39:55 -0500

    intento de hamburguesa

--- js/animaciones.js
+++ js/animaciones.js
@@ -15,6 +15,10 @@ document.addEventListener("DOMContentLoaded", function () {
   // Funcionalidad para mostrar/ocultar detalles de servicios
   setupServicesToggle();
 
+
+
+
+  
   // Animación para el botón CTA
   const ctaButton = document.querySelector(".cta-button");
   if (ctaButton) {
@@ -741,4 +745,25 @@ document.addEventListener('DOMContentLoaded', () => {
   prevBtn.addEventListener('click', prevSlide);
 
   setInterval(nextSlide, 4000); // Cambio automático
+});
+
+
+const menuBtn = document.getElementById('menu-btn');
+const closeBtn = document.getElementById('close-btn');
+const menu = document.getElementById('avianca-menu');
+const overlay = document.getElementById('overlay');
+
+menuBtn.addEventListener('click', () => {
+  menu.classList.add('open');
+  overlay.classList.add('show');
+});
+
+closeBtn.addEventListener('click', () => {
+  menu.classList.remove('open');
+  overlay.classList.remove('show');
+});
+
+overlay.addEventListener('click', () => {
+  menu.classList.remove('open');
+  overlay.classList.remove('show');
 });
